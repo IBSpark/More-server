ae2e489563ff6df91582e78040b0ed056d3dee82
IBSpark <ibadweb68@gmail.com>
2026-02-12 18:45:21 +0500

    Change code of sign up

--- api/signup.js
+++ api/signup.js
@@ -1,11 +1,12 @@
-import express from "express";
 import bcrypt from "bcryptjs";
 import connectDB from "../lib/db.js";
 import User from "../models/user.js";
 
-const router = express.Router();
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
 
-router.post("/signup", async (req, res) => {
   try {
     await connectDB();
 
@@ -15,24 +16,30 @@ router.post("/signup", async (req, res) => {
       return res.status(400).json({ message: "All fields required" });
     }
 
-    const exists = await User.findOne({ email });
-    if (exists) {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await User.create({
+    const newUser = await User.create({
       name,
       email,
       password: hashedPassword,
     });
 
-    return res.status(201).json({ message: "Signup successful" });
+    return res.status(201).json({
+      message: "Signup successful",
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
+
   } catch (error) {
     console.error("Signup error:", error);
     return res.status(500).json({ message: "Internal Server Error" });
   }
-});
-
-export default router;
+}
